4a93f4d055990729b31c35b419e2a86ed1bd76e1
munishchouhan <hrma017@gmail.com>
2023-12-12 19:38:13 +0000

    wave v1.1.0

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -1,19 +1,38 @@
-# Generated with JReleaser 1.9.0 at 2023-12-12T18:35:41.007737982Z
+# Generated with JReleaser 1.9.0 at 2023-12-12T19:38:00.427703804Z
 class Wave < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip", :using => :nounzip
+  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
   version "1.1.0"
-  sha256 "c4b4ef3c564af089a19a79f785bb6e7248140edb64dbf37ecbba322afe44c006"
   license "MPL-2.0"
 
+    if OS.mac? && Hardware::CPU.arm?
+        url "https://github.com/seqeralabs/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-arm64.zip"
+        sha256 "91743e75c5a42c95a318df00b029ecb3d468cc58cd4282f3b8702ffd7f1ea55a"
 
-  def install
-    bin.install "wave" => "wave"
-  end
+        def install
+            bin.install "wave" => "wave"
+        end
 
-  test do
-    output = shell_output("#{bin}/wave --version")
-    assert_match "1.1.0", output
-  end
+        test do
+            output = shell_output("#{bin}/wave --version")
+            assert_match "1.1.0", output
+        end
+    end
+
+    if OS.mac? && Hardware::CPU.intel?
+        url "https://github.com/seqeralabs/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
+        sha256 "91743e75c5a42c95a318df00b029ecb3d468cc58cd4282f3b8702ffd7f1ea55a"
+
+        def install
+            bin.install "wave" => "wave"
+        end
+
+        test do
+            output = shell_output("#{bin}/wave --version")
+            assert_match "1.1.0", output
+        end
+    end
+
+
 end
