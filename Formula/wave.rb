888c86b406944a9291e53dd748e6bd3961bdb8f8
munishchouhan <hrma017@gmail.com>
2023-12-12 17:51:44 +0100

    changed formula

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -15,7 +15,7 @@ class Wave < Formula
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "42cecd2f1a3b08231ad8c8052e2573439b4c644ac7555997d38a1a3907c4bd0"
+    sha256 "42cecd2f1a3b08231ad8c8052e2573439b4c644ac7555997d38a1a3907c4bd00"
 
     def install
       bin.install "wave-1.1.0-macos-x86_64.zip" => "wave"
