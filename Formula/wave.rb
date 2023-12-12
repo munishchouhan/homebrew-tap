e029dc59e035b33c14e8085af003e5d20555a20a
jreleaserbot <jreleaser@kordamp.org>
2023-12-12 19:52:32 +0000

    wave v1.1.0

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -1,37 +1,38 @@
-# Generated with JReleaser 1.9.0 at 2023-12-12T19:38:00.427703804Z
+# Generated with JReleaser 1.9.0 at 2023-12-12T19:52:13.491266847Z
 class Wave < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
   version "1.1.0"
   license "MPL-2.0"
 
-    if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-        sha256 "91743e75c5a42c95a318df00b029ecb3d468cc58cd4282f3b8702ffd7f1ea55a"
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-linux-x86_64"
+    sha256 "31eb9bbd9a134e0322c80dc37ab955b3f746ee7265194ea69a85bfe3258e0fb2"
 
-        def install
-            bin.install "wave" => "wave"
-        end
-
-        test do
-            output = shell_output("#{bin}/wave --version")
-            assert_match "1.1.0", output
-        end
+    def install
+      bin.install "wave-1.1.0-linux-x86_64" => "wave"
     end
+  end
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-osx-x86_64.zip"
+    sha256 "481a3b9e65002b48e9fba26e474bdf61adceddb3903e8c34252d115c3589fac6"
 
-    if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-        sha256 "91743e75c5a42c95a318df00b029ecb3d468cc58cd4282f3b8702ffd7f1ea55a"
-
-        def install
-            bin.install "wave" => "wave"
-        end
-
-        test do
-            output = shell_output("#{bin}/wave --version")
-            assert_match "1.1.0", output
-        end
+    def install
+      bin.install "wave-1.1.0-osx-x86_64" => "wave"
     end
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-osx-x86_64.zip"
+    sha256 "481a3b9e65002b48e9fba26e474bdf61adceddb3903e8c34252d115c3589fac6"
+
+    def install
+      bin.install "wave-1.1.0-osx-x86_64" => "wave"
+    end
+  end
 
 
+  test do
+    output = shell_output("#{bin}/wave --version")
+    assert_match "1.1.0", output
+  end
 end
