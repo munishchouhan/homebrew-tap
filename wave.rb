dc4324521d6e2bf444f619b4bf573f1f74bc3df5
munishchouhan <hrma017@gmail.com>
2023-12-12 16:17:58 +0100

    chnaged formula

--- wave.rb
+++ wave.rb
@@ -4,7 +4,7 @@
 class Wave < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  url "https://github.com/seqeralabs/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-arm64.zip"
+  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-arm64.zip"
   version "1.1.0"
   sha256 "6ce417f40c87f52459b2fe9275223732b3ff666398279fc9ab165864b9cdc34e"
   license "MPL-2.0"
