b364bf3952abbd745216a4b1337e18881b7e8e3a
munishchouhan <hrma017@gmail.com>
2023-12-12 16:26:11 +0000

    wave v1.1.0

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.9.0 at 2023-12-12T16:04:16.191249436Z
+# Generated with JReleaser 1.9.0 at 2023-12-12T16:25:49.902950196Z
 class Wave < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
@@ -6,32 +6,32 @@ class Wave < Formula
   license "MPL-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "5bce9d39c50e402a1333beb403a37290758b081e6188a6c5f18b622aae17bcf6"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-linux-x86_64"
+    sha256 "ce23977e56b8e14a890d993bfba6a642eef400e7d8eb218aadddd28eb67b226d"
 
     def install
       bin.install "wave-1.1.0-linux-x86_64" => "wave"
     end
   end
-  if OS.mac? && Hardware::CPU.arm?
+  if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "38360b23ec2ebf732d235e0893e0a4303e8b68b0d405ac008a96a0f75e71edd0"
+    sha256 "a31248098f83998dca84088014c9914ba85ecb8f779941e4ecd81c3c6e9e6fda"
 
     def install
       bin.install "wave-1.1.0-macos-x86_64" => "wave"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "be537ffde066e9a3788a517e8c2903af751af61430ac8292f6f59cc3b80633d0"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64"
+    sha256 "299898067063c38646ecffa43afd4548386ea817042a0e45568042c026772126"
 
     def install
       bin.install "wave-1.1.0-macos-x86_64" => "wave"
     end
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "be537ffde066e9a3788a517e8c2903af751af61430ac8292f6f59cc3b80633d0"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64"
+    sha256 "299898067063c38646ecffa43afd4548386ea817042a0e45568042c026772126"
 
     def install
       bin.install "wave-1.1.0-macos-x86_64" => "wave"
