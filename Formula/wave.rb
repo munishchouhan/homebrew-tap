b585c83c85c7995d2db049d1bc9c34f27d1af613
munishchouhan <hrma017@gmail.com>
2023-12-12 16:44:32 +0100

    changed formula

--- Formula/wave.rb
+++ Formula/wave.rb
@@ -2,7 +2,7 @@
 class Wave < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip.zip", :using => :nounzip
+  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip", :using => :nounzip
   version "1.1.0"
   sha256 "e3ef90a6ee35a34da3a7d6194bd0c15ba621d6d38c555d94799304a71c21bbb3"
   license "MPL-2.0"
