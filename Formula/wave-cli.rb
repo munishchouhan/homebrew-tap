05e26b712129d13bb50c670fabcbe95a70bb1016
jreleaserbot <jreleaser@kordamp.org>
2023-12-19 15:07:45 +0000

    wave-cli v1.1.0

--- Formula/wave-cli.rb
+++ Formula/wave-cli.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.9.0 at 2023-12-19T14:54:54.730834796Z
+# Generated with JReleaser 1.9.0 at 2023-12-19T15:07:30.594852351Z
 class WaveCli < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
@@ -7,11 +7,11 @@ class WaveCli < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-arm64.zip"
-    sha256 "b138f6c5126773319961e0de3c8a1e05e2f6c1ec7f956367dffff2f30c34fe39"
+    sha256 "1eaeb78a82f0f476f0acbce49a6ad7a5bd3cb3a7c2c98e57fc9abf98e5f94ace"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.1.0/wave-1.1.0-macos-x86_64.zip"
-    sha256 "fb76618ab26c8a563f1a141b9b1c43b29d6d51067a5270a91eb827cb86403df5"
+    sha256 "5ed1efec551d9c0cba9fa1496b26b4cb39da0ed3fa8e1336526e306b6a169e93"
   end
 
 
