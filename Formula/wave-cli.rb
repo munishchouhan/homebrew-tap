05cb3919568307084b4457f4cdfc8855297536d3
jreleaserbot <jreleaser@kordamp.org>
2024-07-10 12:58:36 +0000

    wave-cli v1.4.2

--- Formula/wave-cli.rb
+++ Formula/wave-cli.rb
@@ -1,39 +1,39 @@
-# Generated with JReleaser 1.11.0 at 2024-04-11T13:45:36.640988459Z
+# Generated with JReleaser 1.13.1 at 2024-07-10T12:58:19.733834513Z
 
 class WaveCli < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  version "1.3.2"
+  version "1.4.2"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.3.2/wave-1.3.2-linux-x86_64", :using => :nounzip
-    sha256 "9dd151318200e3bcfe0b2be64e67a22d4e02018b6c7a13e755f70bf1157f048b"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-linux-x86_64", :using => :nounzip
+    sha256 "851ebec9067d0ad08dd8b3ecf212ee99ce7c3d409fd5335e641b042c68d8b69d"
 
     def install
-      bin.install "wave-1.3.2-linux-x86_64" => "wave"
-    end
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.3.2/wave-1.3.2-macos-arm64", :using => :nounzip
-    sha256 "f454d91157ebc46a6f0685bcb6e076fbae959b38783ca49b19dc3630b6129132"
-
-    def install
-      bin.install "wave-1.3.2-macos-arm64" => "wave"
+      bin.install "wave-1.4.2-linux-x86_64" => "wave"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.3.2/wave-1.3.2-macos-x86_64", :using => :nounzip
-    sha256 "c43c3207825f242c7de9e3c1ec2859370cba7a5d2b569d45c3c241a36fb5ef35"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-macos-x86_64", :using => :nounzip
+    sha256 "cf8b2bd58d85209896f2cfba7121f9fecf434d19517bc2d93f6fec9270f3c47a"
 
     def install
-      bin.install "wave-1.3.2-macos-x86_64" => "wave"
+      bin.install "wave-1.4.2-macos-x86_64" => "wave"
+    end
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-macos-x86_64", :using => :nounzip
+    sha256 "cf8b2bd58d85209896f2cfba7121f9fecf434d19517bc2d93f6fec9270f3c47a"
+
+    def install
+      bin.install "wave-1.4.2-macos-x86_64" => "wave"
     end
   end
 
 
   test do
     output = shell_output("#{bin}/wave --version")
-    assert_match "1.3.2", output
+    assert_match "1.4.2", output
   end
 end
