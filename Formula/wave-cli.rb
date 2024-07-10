15e4ebb0971c01d59eccbfc5f1f0a6a4c72c1f68
jreleaserbot <jreleaser@kordamp.org>
2024-07-10 13:27:15 +0000

    wave-cli v1.4.3

--- Formula/wave-cli.rb
+++ Formula/wave-cli.rb
@@ -1,39 +1,39 @@
-# Generated with JReleaser 1.13.1 at 2024-07-10T12:58:19.733834513Z
+# Generated with JReleaser 1.13.1 at 2024-07-10T13:27:01.596325881Z
 
 class WaveCli < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  version "1.4.2"
+  version "1.4.3"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-linux-x86_64", :using => :nounzip
-    sha256 "851ebec9067d0ad08dd8b3ecf212ee99ce7c3d409fd5335e641b042c68d8b69d"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.3/wave-1.4.3-linux-x86_64", :using => :nounzip
+    sha256 "f53f75e0f96fd426626665f1537abc31f28b69c3d4c5b7160cc795938ee3fa37"
 
     def install
-      bin.install "wave-1.4.2-linux-x86_64" => "wave"
+      bin.install "wave-1.4.3-linux-x86_64" => "wave"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-macos-x86_64", :using => :nounzip
-    sha256 "cf8b2bd58d85209896f2cfba7121f9fecf434d19517bc2d93f6fec9270f3c47a"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.3/wave-1.4.3-macos-x86_64", :using => :nounzip
+    sha256 "ed815c07557ff2106faea387174679d09577e2f22e21d8d351e9b6cc6a68afea"
 
     def install
-      bin.install "wave-1.4.2-macos-x86_64" => "wave"
+      bin.install "wave-1.4.3-macos-x86_64" => "wave"
     end
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.2/wave-1.4.2-macos-x86_64", :using => :nounzip
-    sha256 "cf8b2bd58d85209896f2cfba7121f9fecf434d19517bc2d93f6fec9270f3c47a"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.4.3/wave-1.4.3-macos-x86_64", :using => :nounzip
+    sha256 "ed815c07557ff2106faea387174679d09577e2f22e21d8d351e9b6cc6a68afea"
 
     def install
-      bin.install "wave-1.4.2-macos-x86_64" => "wave"
+      bin.install "wave-1.4.3-macos-x86_64" => "wave"
     end
   end
 
 
   test do
     output = shell_output("#{bin}/wave --version")
-    assert_match "1.4.2", output
+    assert_match "1.4.3", output
   end
 end
