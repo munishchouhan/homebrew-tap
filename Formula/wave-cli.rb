e3636285b4224c45ec2c5af34c269a509a21674f
jreleaserbot <jreleaser@kordamp.org>
2024-10-15 03:16:02 +0000

    wave-cli v1.5.0-rc.4

--- Formula/wave-cli.rb
+++ Formula/wave-cli.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.14.0 at 2024-10-15T02:35:32.112793259Z
+# Generated with JReleaser 1.14.0 at 2024-10-15T03:15:50.222568849Z
 
 class WaveCli < Formula
   desc "Wave CLI"
@@ -8,7 +8,7 @@ class WaveCli < Formula
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-linux-x86_64", :using => :nounzip
-    sha256 "72b0958a8019415d903d47cd4bdacd2155445f4cf3157d7f6461ddaf5805669b"
+    sha256 "ad9aadf91f9b8b426b21199b1f26d0f9d08c351e348debcd115056e8ef6b5287"
 
     def install
       bin.install "wave-1.5.0-rc.4-linux-x86_64" => "wave"
@@ -16,7 +16,7 @@ class WaveCli < Formula
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-macos-x86_64", :using => :nounzip
-    sha256 "6002349d76ee412088e004733ae87052d821c4151084a06add4c497880718de6"
+    sha256 "cd40ce4daeed6d0e36a5f2408a86df0ac0eb7bb4dc63143d16cb67e9e2841c77"
 
     def install
       bin.install "wave-1.5.0-rc.4-macos-x86_64" => "wave"
@@ -24,7 +24,7 @@ class WaveCli < Formula
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-macos-x86_64", :using => :nounzip
-    sha256 "6002349d76ee412088e004733ae87052d821c4151084a06add4c497880718de6"
+    sha256 "cd40ce4daeed6d0e36a5f2408a86df0ac0eb7bb4dc63143d16cb67e9e2841c77"
 
     def install
       bin.install "wave-1.5.0-rc.4-macos-x86_64" => "wave"
