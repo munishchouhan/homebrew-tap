8ffdd5726d8bd31e1d0dfad7e76b420222589028
jreleaserbot <jreleaser@kordamp.org>
2024-10-15 04:22:21 +0000

    wave-cli v1.5.0

--- Formula/wave-cli.rb
+++ Formula/wave-cli.rb
@@ -1,39 +1,39 @@
-# Generated with JReleaser 1.14.0 at 2024-10-15T03:15:50.222568849Z
+# Generated with JReleaser 1.14.0 at 2024-10-15T04:22:05.61215871Z
 
 class WaveCli < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  version "1.5.0-rc.4"
+  version "1.5.0"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-linux-x86_64", :using => :nounzip
-    sha256 "ad9aadf91f9b8b426b21199b1f26d0f9d08c351e348debcd115056e8ef6b5287"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0/wave-1.5.0-linux-x86_64", :using => :nounzip
+    sha256 "f40fe102b75a170a55c29a0ac041f230ed07454eeb43ee6fd55db56b73ca5e04"
 
     def install
-      bin.install "wave-1.5.0-rc.4-linux-x86_64" => "wave"
+      bin.install "wave-1.5.0-linux-x86_64" => "wave"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-macos-x86_64", :using => :nounzip
-    sha256 "cd40ce4daeed6d0e36a5f2408a86df0ac0eb7bb4dc63143d16cb67e9e2841c77"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0/wave-1.5.0-macos-x86_64", :using => :nounzip
+    sha256 "b9bc8357227ea529aec951c7d0edb50370ba8e93ba98eec15b59b26450b87e1d"
 
     def install
-      bin.install "wave-1.5.0-rc.4-macos-x86_64" => "wave"
+      bin.install "wave-1.5.0-macos-x86_64" => "wave"
     end
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4-macos-x86_64", :using => :nounzip
-    sha256 "cd40ce4daeed6d0e36a5f2408a86df0ac0eb7bb4dc63143d16cb67e9e2841c77"
+    url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0/wave-1.5.0-macos-x86_64", :using => :nounzip
+    sha256 "b9bc8357227ea529aec951c7d0edb50370ba8e93ba98eec15b59b26450b87e1d"
 
     def install
-      bin.install "wave-1.5.0-rc.4-macos-x86_64" => "wave"
+      bin.install "wave-1.5.0-macos-x86_64" => "wave"
     end
   end
 
 
   test do
     output = shell_output("#{bin}/wave --version")
-    assert_match "1.5.0-rc.4", output
+    assert_match "1.5.0", output
   end
 end
