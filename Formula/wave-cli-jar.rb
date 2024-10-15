da3a5ca3b1e7e60384d7de6c466e005dd51ac01e
jreleaserbot <jreleaser@kordamp.org>
2024-10-15 04:22:23 +0000

    wave-cli-jar v1.5.0

--- Formula/wave-cli-jar.rb
+++ Formula/wave-cli-jar.rb
@@ -1,30 +1,30 @@
-# Generated with JReleaser 1.14.0 at 2024-10-15T03:15:50.222568849Z
+# Generated with JReleaser 1.14.0 at 2024-10-15T04:22:05.61215871Z
 
 class WaveCliJar < Formula
   desc "Wave CLI"
   homepage "https://github.com/seqeralabs/wave-cli"
-  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0-rc.4/wave-1.5.0-rc.4.jar", :using => :nounzip
-  version "1.5.0-rc.4"
-  sha256 "e1d4cbb44b1c050701ffe5c9c9e3389d572c1a1a55afaa914c4a0366956d09d4"
+  url "https://github.com/munishchouhan/wave-cli/releases/download/v1.5.0/wave-1.5.0.jar", :using => :nounzip
+  version "1.5.0"
+  sha256 "74e04a9ee23fda6894dccea3458965271d889d4b6421811437523e619a2873c7"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
 
   def install
-    libexec.install "wave-1.5.0-rc.4.jar"
+    libexec.install "wave-1.5.0.jar"
 
     bin.mkpath
     File.open("#{bin}/wave-cli-jar", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/wave-1.5.0-rc.4.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/wave-1.5.0.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/wave-cli-jar --version")
-    assert_match "1.5.0-rc.4", output
+    assert_match "1.5.0", output
   end
 end
